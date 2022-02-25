73c025c5e0e73a208fea26d6c40edef142b71e25
Emanuel Forlin <emaa.forlin@gmail.com>
2022-02-24 21:56:04 -0300

    Added create table if not exists

--- README.md
+++ README.md
@@ -12,16 +12,21 @@ Soon available...
 
 ### Changelog
 
+#### Other features
+- Now it creates the table if not exists 
+- More error handling
+- New graceful shutdown method
+- Added reusing deleted IDs
+- Use a database
+
 #### All CRUD operations can be performed
 - Delete products `DELETE /products/{id}` 
 - Update products `PUT /products/{id}`
 - Add products `POST /products`
 - Retrieve products `GET /products`
+#### Docs
+- API Documentation in `GET /docs`
 
-#### Other features
-- More error handling
-- New graceful shutdown method
-- Added reusing deleted IDs
-- Use a database
+
 
  ## Note for my self: Clean the code before merge it into main 
--- database/database.go
+++ database/database.go
@@ -15,6 +15,18 @@ type Connection struct {
 	dbname string
 }
 
+const tableScript string = `CREATE TABLE JIS.products (
+	ID int unsigned NOT NULL,
+	Name varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+	Description varchar(256) DEFAULT NULL,
+	Price float DEFAULT NULL,
+	SKU varchar(8) DEFAULT NULL,
+	created_at timestamp NULL DEFAULT NULL,
+	updated_at timestamp NULL DEFAULT NULL,
+	deleted_at timestamp NULL DEFAULT NULL,
+	PRIMARY KEY (ID) USING BTREE
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;`
+
 func NewConnection(h string, p int, user string, pass string, db string) *Connection {
 	return &Connection{h, p, user, pass, db}
 }
@@ -22,5 +34,14 @@ func NewConnection(h string, p int, user string, pass string, db string) *Connec
 func (c *Connection) Connect() (*gorm.DB, error) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&autocommit=true&parseTime=True&loc=Local", c.user, c.pass, c.host, c.port, c.dbname)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if !checkTableExists("products", db) {
+		db.Exec(tableScript)
+	}
 	return db, err
+}
+
+func checkTableExists(tableName string, db *gorm.DB) bool {
+	var out string
+	db.Raw("SHOW TABLES;").Row().Scan(&out)
+	return out == tableName
 }
