9b4ec723e3f2f5b5eb192103de48bce0bc050c55
Emanuel Forlin <emaa.forlin@gmail.com>
2022-02-24 21:21:54 -0300

    Creates a fake UpdatedAt timestamp when creating prod

--- data/addProduct.go
+++ data/addProduct.go
@@ -17,7 +17,7 @@ func AddProduct(p *Product, db *gorm.DB) error {
 		Price:       p.Price,
 		SKU:         p.SKU,
 		CreatedAt:   p.CreatedAt,
-		UpdatedAt:   p.UpdatedAt,
+		UpdatedAt:   time.Date(1986, time.January, 1, 0, 0, 0, 0, time.Local),
 	}
 	if prod.ID != 0 && prod.Name != "" && prod.Price != 0 {
 		db.Create(&prod)
